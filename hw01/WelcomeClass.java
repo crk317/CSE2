66d36e46271ec5d3cbac51019116d4e10b9b2c29
crk317 <crk317@lehigh.edu>
2015-01-28 03:02:11 +0000

    WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,22 @@
+//Clarisa Kelly
+// HW 01
+// Welcome Class Java Program
+
+// define a class
+public class WelcomeClass {
+
+// add main method
+public static void main(String[] args) {
+    
+    //Print out Welcome Class with Lehigh ID
+    System.out.println("-----------");
+    System.out.println("| WELCOME |");
+    System.out.println("-----------");
+    System.out.println(" ^  ^  ^  ^  ^  ^");
+    System.out.println("/ \/ \/ \/ \/ \/ \");
+    System.out.println("<-C--R--K--3--1--7->");
+    System.out.println("\ /\ /\ /\ /\ /\ /");
+    System.out.println(" v  v  v  v  v  v");
+    System.out.println("This weekend I am going to compete in the semifinals of the International Collegiate A Capella Campionship.");
+}
+}

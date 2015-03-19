a6ebf753ad3c7926d5807ed821f73b70ca8dd5ea
crk317 <crk317@lehigh.edu>
2015-03-18 21:27:51 -0400

    Update and rename Waves.java to waves.java

--- hw07/waves.java
+++ hw07/waves.java
@@ -4,7 +4,7 @@
 
 import java.util.Scanner;
 
-public class Waves{
+public class waves{
     public static void main(String[] args) {
     Scanner myScanner = new Scanner(System.in);
     System.out.print("Enter an integer between 1 and 30: ");
